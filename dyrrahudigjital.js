7d454376c5624c13f8ceb4601320481e7965beb1
mnaglak <51410453+mnaglak@users.noreply.github.com>
2023-11-26 14:55:46 -0500

    aaa

--- dyrrahudigjital.js
+++ dyrrahudigjital.js
@@ -21,7 +21,7 @@ var popUpWidth = mapWidth * 0.6;
 var popUpHeight = mapHeight * 0.6;
 var imageWidth = popUpWidth * 0.8;
 var imageHeight = imageWidth * 0.6;
-
+var langNumber=0;
 var  Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {maxZoom:22, maxNativeZoom:19,
 attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
   }).addTo(map);
@@ -30,14 +30,14 @@ var allSUs = L.geoJSON(allLayers, {
   onEachFeature: function (feature, layer) {
     var out = [];
       if (feature.properties){
-        out.push("SU Number: " + feature.properties.SU);
+        out.push("SU: " + feature.properties.SU);
           }
     layer.bindPopup(out.join("<br />"), {closeOnClick:true});
 
   }
 }).addTo(map);
 var sites= L.geoJSON(nearbySites, {
-  onEachFeature: popUpPlaces,
+  onEachFeature: popUpPlacesAL,
   pointToLayer: function (feature, latlng) {
       var markerStyle = {
           color: "#FFF",
@@ -50,7 +50,7 @@ var sites= L.geoJSON(nearbySites, {
   }
 }).addTo(map);
 
-function popUpPlaces(f, l) {
+function popUpPlacesAL(f, l) {
     l.bindTooltip(f.properties.Name);
     var out = [];
     var myImage;
@@ -59,14 +59,62 @@ function popUpPlaces(f, l) {
     if (f.properties) {
         out.push("<img class='center' style='max-width:300px' src= '.\\" + f.properties.photo + "'/>");
         out.push("<b><u>" + f.properties.Name + '</u></b>');
-        out.push('<br><b>Description: </b>' + f.properties.descriptionEN + '<br><center>');
+        out.push( f.properties.descriptionAL);
         l.bindPopup(out.join("<br/>"), { maxHeight: popUpHeight, maxWidth: popUpWidth, closeOnClick: true });
     }
 }
-
+function popUpPlacesEN(f, l) {
+    l.bindTooltip(f.properties.Name);
+    var out = [];
+    var myImage;
+    var myImageW = imageWidth;
+    var myImageH = imageHeight;
+    if (f.properties) {
+        out.push("<img class='center' style='max-width:300px' src= '.\\" + f.properties.photo + "'/>");
+        out.push("<b><u>" + f.properties.Name + '</u></b>');
+        out.push( f.properties.descriptionEN);
+        l.bindPopup(out.join("<br/>"), { maxHeight: popUpHeight, maxWidth: popUpWidth, closeOnClick: true });
+    }
+}
 var drone1 = L.tileLayer('./drone/{z}/{x}/{y}.png', {tms: true, attribution: "", minZoom: 18, maxZoom: 22}).addTo(map);
 map.on('popupopen', function(e) {
     var px = map.project(e.target._popup._latlng); // find the pixel location on the map where the popup anchor is
     px.y -= e.target._popup._container.clientHeight/2; // find the height of the popup container, divide by 2, subtract from the Y axis of marker location
     map.panTo(map.unproject(px),{animate: true}); // pan to new center
 });
+
+function changeLanguage(lang) {
+  map.removeLayer(sites);
+  if (lang == "en") {
+    langNumber=1;
+    var sites= L.geoJSON(nearbySites, {
+      onEachFeature: popUpPlacesEN,
+      pointToLayer: function (feature, latlng) {
+          var markerStyle = {
+              color: "#FFF",
+              fillOpacity: 1,
+              opacity: 0.5,
+              weight: 1,
+              radius: 10
+          };
+          return L.circleMarker(latlng, markerStyle);
+      }
+
+    })}
+    else {
+      langNumber=1;
+      var sites= L.geoJSON(nearbySites, {
+        onEachFeature: popUpPlacesAL,
+        pointToLayer: function (feature, latlng) {
+            var markerStyle = {
+                color: "#FFF",
+                fillOpacity: 1,
+                opacity: 0.5,
+                weight: 1,
+                radius: 10
+            };
+            return L.circleMarker(latlng, markerStyle);
+        }
+    })}
+    sites.addTo(map);
+}
